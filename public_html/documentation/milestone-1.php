128f0bb886f1d368907ff74210bfc2f613a7718e
Tanisha Purnell <tanishad93@gmail.com>
2017-10-15 10:56:00 -0700

    created milestone.php

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>data-design</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<img src="" width="450" height="350" alt="">
+		<h3>Need to know</h3>
+		<p></p>
+		<h3>Demographics</h3>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+		<h3>Goals</h3>
+		<ul>
+			<li></li>
+			<li></li>
+		</ul>
+		<h3>User Story</h3>
+		<p></p>
+		<br>
+		<h3>Use Case/ Interaction Flow</h3>
+		<p></p>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</body>
+</html>
