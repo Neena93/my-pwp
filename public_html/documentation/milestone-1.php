0abd61d15dc541ebb5907fc78092593d0813a91b
Tanisha Purnell <tanishad93@gmail.com>
2017-10-18 21:55:04 -0700

    Added img

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,20 +7,25 @@
 	<body>
 		<h1>Purpose, Audience & Goal</h1>
 		<ul>
-			<li><strong>Purpose:</strong>I am using this project to promote myself as a software developer to potential employers.</li>
-			<li><strong>Audience:</strong>This project aims to target collaborators, start ups, potential employers or even someone who is interested in having a website created.</li>
-			<li><strong>Goal:</strong>This project will target potential employers with the primary goal of displaying my correct skill level and means to contact me via email, social media platorms. It will also display my GitHub account in case an potential employer is intersted in checking my recent commits.</strong></li>
+			<li><strong>Purpose:</strong> I am using this project to promote myself as a software developer to potential employers.</li>
+			<br>
+			<li><strong>Audience:</strong> This project aims to target collaborators, start ups, potential employers or even someone who is interested in having a website created.</li>
+			<br>
+			<li><strong>Goal:</strong> This project will target potential employers with the primary goal of displaying my correct skill level and means to contact me via email, social media platorms. It will also display my GitHub account in case an potential employer is intersted in checking my recent commits.</strong></li>
 		</ul>
 		<h3>Persona</h3>
-		<img src="" width="450" height="350" alt="">
+		<img src="/images/issa-rae.jpg" width="450" height="350" alt="IssaRae">
 		<ul>
-			<li><strong>Name:</strong>Felicitas Bradley</li>
-			<li><strong>Age:</strong>37</li>
+			<li><strong>Name:</strong> Felicitas Bradley</li>
+			<li><strong>Age:</strong> 37</li>
 			<br>
-			<li><strong>Profession:</strong>Manager at KittyCodeABQ in search of a full-time junior developer who specializes in creating databases.</li>
-			<li><strong>Technology</strong>Felicitas has a Pixel XL, running Oreo  and her prefered web browser is Orfox. Her prefered PC is Mac while in the office and while home she prefers her self-made desktop computer. </li>
-			<li><strong>Attitudes and Behaviors:</strong>Short staffed, stressed because staff are overworked. Felicitas does not have much time outside of work to view and print individual resumes in order to find an employee. However, Felicitas is able to view digital websites as she is a frequent web browser and heavy social media websites.</li>
-			<li><strong>Frustrations and Needs:</strong>Felicitas has a deadline of 2 weeks to hire 3 backend developers with at least 3-4 years experience or someone with less years experience but are able to prove they have comparable knowlegde of the job. She is also interested in individuals who have excellent soft skills and are able to adapt and learn. </li>
+			<li><strong>Profession:</strong> Manager at KittyCodeABQ in search of a full-time junior developer who specializes in creating databases.</li>
+			<br>
+			<li><strong>Technology:</strong> Felicitas has a Pixel XL, running Oreo  and her prefered web browser is Orfox. Her prefered PC is Mac while in the office and while home she prefers her self-made desktop computer. </li>
+			<br>
+			<li><strong>Attitudes and Behaviors:</strong> Short staffed, stressed because staff are overworked. Felicitas does not have much time outside of work to view and print individual resumes in order to find an employee. However, Felicitas is able to view digital websites as she is a frequent web browser and heavy social media websites.</li>
+			<br>
+			<li><strong>Frustrations and Needs:</strong> Felicitas has a deadline of 2 weeks to hire 3 backend developers with at least 3-4 years experience or someone with less years experience but are able to prove they have comparable knowlegde of the job. She is also interested in individuals who have excellent soft skills and are able to adapt and learn. </li>
 		</ul>
 		<h3>User Story</h3>
 		<p></p>
