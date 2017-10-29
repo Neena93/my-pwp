150c8e7ef9f8dbbcef2b628c4391205b557c5e65
Tanisha Purnell <tanishad93@gmail.com>
2017-10-29 10:57:18 -0700

    updated milestone-2.php

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="US-en">
+	<head>
+		<title>Content Strategy</title>
+	</head>
+		<body>
+			<h1>Content Strategy</h1>
+				<p>My website will be a one page portfolio.</p>
+				<p><strong>Welcome:</strong>This section will welcome users to my portfolio with a one sentence creating stating my name. The welcome section will be presented over a photo I place in the background.</p>
+				<p><strong>About Me:</strong>This section will present me as an aspiring web developer and direct users to take a look at my portfolio as well as state my contact information if user is interested in contacting me. This section will also display my soft skills as well as my development skills.</p>
+				<p><strong>Portfolio:</strong>This section will be a image that a user can click to be directed to a physical layout of my current work and my contributions to the work will be outlined as well.</p>
+				<p><strong>Contact:</strong>This section will include button links for users to directly follow me on my social media as well as a input option for comments and inquires.</p>
+		</body>
+</html>
