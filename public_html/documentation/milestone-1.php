504db24529e67272d1e8cc7dc341d9ea9d9cb52e
Tanisha Purnell <tanishad93@gmail.com>
2017-10-29 15:46:45 -0700

    updated milestone-2.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,30 +6,26 @@
 	</head>
 	<body>
 		<h1>Purpose, Audience and Goal</h1>
-		<ul>
-			<li><strong>Purpose:</strong> I am using this project to promote myself as a software developer to potential employers.</li>
+			<p><strong>Purpose:</strong> I am using this project to promote myself as a software developer to potential employers.</p>
 			<br>
-			<li><strong>Audience:</strong> This project aims to target collaborators, start ups, potential employers or even someone who is interested in having a website created.</li>
+			<p><strong>Audience:</strong> This project aims to target collaborators, start ups, potential employers or even someone who is interested in having a website created.</p>
 			<br>
-			<li><strong>Goal:</strong> This project will target potential employers with the primary goal of displaying my current skill level and means to contact me via email and social media platforms. It will also display my GitHub account in case an potential employer is intersted in checking my recent commits.</strong></li>
-		</ul>
+			<p><strong>Goal:</strong> This project will target potential employers with the primary goal of displaying my current skill level and means to contact me via email and social media platforms. It will also display my GitHub account in case an potential employer is intersted in checking my recent commits.</strong></p>
 		<h3>Persona</h3>
 		<img src ="images/issa-rae-edit.jpg" width="400" height="400" alt="Issa Rae">
-		<ul>
-			<li><strong>Name:</strong> Felicitas Bradley</li>
-			<li><strong>Age:</strong> 37</li>
+			<p><strong>Name:</strong> Felicitas Bradley</p>
+			<p><strong>Age:</strong> 37</p>
 			<br>
-			<li><strong>Profession:</strong> Manager at KittyCodeABQ in search of a full-time junior developer who specializes in creating databases.</li>
+			<p><strong>Profession:</strong> Manager at KittyCodeABQ in search of a full-time junior developer who specializes in creating databases.</p>
 			<br>
-			<li><strong>Technology:</strong> Felicitas has a Pixel XL, running Oreo  and her choice web browser is Orfox. Her prefered PC is Mac while in the office and while home she prefers her self-made desktop computer. </li>
+			<p><strong>Technology:</strong> Felicitas has a Pixel XL, running Oreo  and her choice web browser is Orfox. Her prefered PC is Mac while in the office and while home she prefers her self-made desktop computer. </p>
 			<br>
-			<li><strong>Attitudes and Behaviors:</strong> Short staffed, stressed because staff are overworked. Felicitas does not have much time outside of work to view and print individual resumes in order to find an employee. However, Felicitas is able to view digital websites as she is a frequent web browser and heavy social media websites.</li>
+			<p><strong>Attitudes and Behaviors:</strong> Short staffed, stressed because staff are overworked. Felicitas does not have much time outside of work to view and print individual resumes in order to find an employee. However, Felicitas is able to view digital websites as she is a frequent web browser and heavy social media websites.</p>
 			<br>
-			<li><strong>Frustrations and Needs:</strong> Felicitas has a deadline of 2 weeks to hire 3 backend developers with at least 3-4 years experience or someone with less years experience but are able to prove they have comparable knowlegde of the job. She is also interested in individuals who have excellent soft skills and are able to adapt and learn.</li>
+			<p><strong>Frustrations and Needs:</strong> Felicitas has a deadline of 2 weeks to hire 3 backend developers with at least 3-4 years experience or someone with less years experience but are able to prove they have comparable knowlegde of the job. She is also interested in individuals who have excellent soft skills and are able to adapt and learn.</p>
 			<br>
-			<li><strong>Goals:</strong> Felicitas plans to meet the 2 week dead line to hire 3 backend developers so her understaffed KittyCodeABQ can expand. </li>
+			<p><strong>Goals:</strong> Felicitas plans to meet the 2 week dead line to hire 3 backend developers so her understaffed KittyCodeABQ can expand. </>
 			<br>
-		</ul>
 		<h3>User Story</h3>
 		<p><strong>User Story:</strong> Felicitas with her busy schedule needs to be able to quickly determine if a candidate is qualified for a backend development job.</p>
 	</body>
