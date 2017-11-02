55c42a7deaa0724b9abc0fe56383e82d91f7d9f1
Tanisha Purnell <tanishad93@gmail.com>
2017-11-01 22:15:18 -0700

    updated milestone-2.php

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html lang="US-en">
 	<head>
-		<title>Content Strategy</title>
+		<meta charset="utf-8"/>
+		<title>Milestone 2</title>>
 	</head>
-		<body>
-			<h1>Content Strategy</h1>
-				<p>My website will be a one page portfolio.</p>
-				<p><strong>Welcome:</strong>This section will welcome users to my portfolio with a one sentence creating stating my name. The welcome section will be presented over a photo I place in the background.</p>
-				<p><strong>About Me:</strong>This section will present me as an aspiring web developer and direct users to take a look at my portfolio as well as state my contact information if user is interested in contacting me. This section will also display my soft skills as well as my development skills.</p>
-				<p><strong>Portfolio:</strong>This section will be a image that a user can click to be directed to a physical layout of my current work and my contributions to the work will be outlined as well.</p>
-				<p><strong>Contact:</strong>This section will include button links for users to directly follow me on my social media as well as a input option for comments and inquires.</p>
+	<body>
+		<h1>Content Strategy</h1>
+		<p>My website will be a one page portfolio.</p>
+		<p><strong>Welcome:</strong>This section will welcome users to my portfolio with a one sentence creating stating my name. The welcome section will be presented over a photo I place in the background.</p>
+		<p><strong>About Me:</strong>This section will present me as an aspiring web developer and direct users to take a look at my portfolio as well as state my contact information if user is interested in contacting me. This section will also display my soft skills as well as my development skills.</p>
+		<p><strong>Portfolio:</strong>This section will be a image that a user can click to be directed to a physical layout of my current work and my contributions to the work will be outlined as well.</p>
+		<p><strong>Contact:</strong>This section will include button links for users to directly follow me on my social media as well as a input option for comments and inquires.</p>
+		<h1><strong>Desktop and Mobile Wire frame</strong></h1>
+		<div class="container">
+			<div class="column">
+				<h2>Desktop</h2>
+				<img src="" alt="Wireframe for desktop"/>
+			</div>
+			<div class="column">
+				<h2>Mobile</h2>
+				<img src="images/mobile-pwp.png" alt="mobile wire frame"/>
+			</div>
+		</div>
 		</body>
 </html>
