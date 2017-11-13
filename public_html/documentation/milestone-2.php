49e4ce2d8d9e43fce3a60e9842533ec391bdde46
Tanisha Purnell <tanishad93@gmail.com>
2017-11-12 17:33:21 -0700

    updated mobile wireframe

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,18 +6,17 @@
 	</head>
 	<body>
 		<h1>Content Strategy</h1>
-		<p>My website will be a one page portfolio.</p>
-		<p><strong>Welcome:</strong>This section will welcome users to my portfolio with a one sentence creating stating my name. The welcome section will be presented over a photo I place in the background.</p>
-		<p><strong>About Me:</strong>This section will present me as an aspiring web developer and direct users to take a look at my portfolio as well as state my contact information if user is interested in contacting me. This section will also display my soft skills as well as my development skills.</p>
-		<p><strong>Portfolio:</strong>This section will be a image that a user can click to be directed to a physical layout of my current work and my contributions to the work will be outlined as well.</p>
-		<p><strong>Contact:</strong>This section will include button links for users to directly follow me on my social media as well as a input option for comments and inquires.</p>
+			<p>My website will be a one page portfolio.</p>
+			<p><strong>Welcome:</strong>This section will welcome users to my portfolio with a one sentence creating stating my name. The welcome section will be presented over a photo I place in the background.</p>
+			<p><strong>About Me:</strong>This section will present me as an aspiring web developer and direct users to take a look at my portfolio as well as state my contact information if user is interested in contacting me. This section will also display my soft skills as well as my development skills.</p>
+			<p><strong>Portfolio:</strong>This section will be a image that a user can click to be directed to a physical layout of my current work and my contributions to the work will be outlined as well.</p>
+			<p><strong>Contact:</strong>This section will include button links for users to directly follow me on my social media as well as a input option for comments and inquires.</p>
 		<h1><strong>Desktop and Mobile Wire frame</strong></h1>
-		<div class="container">
-				<h2>Desktop</h2>
-				<img src="images/desktop-pwp.png" alt="Wireframe for desktop"/>
+			<div class="container">
+					<h2>Desktop</h2>
+					<img src="images/desktop-pwp.png" alt="Wireframe for desktop"/>
 			</div>
-				<h2>Mobile</h2>
-				<img src="images/mobile-pwp.png" alt="mobile wire frame"/>
-		</div>
+		<h2>Mobile</h2>
+			<img src="images/mobile-pwp.png" alt="mobile wire frame"/>
 		</body>
 </html>
