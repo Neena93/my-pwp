44a0a3f5db9cb9cc87b367e7979019013d621a4e
Tanisha Purnell <tanishad93@gmail.com>
2017-11-19 16:44:01 -0700

    created jumbotron

--- public_html/index.php
+++ public_html/index.php
@@ -9,26 +9,67 @@
 
 		<!--Bootstrap Stylesheet-->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<!--CSS Stylesheet-->
+		<link rel="stylesheet" type="text/css" href="/styles/style.css" />
 	</head>
-	<body>
-		<header class="navigation" id="menu-button">
-			<nav class="navbar navbar-inverse navbar-static-top id=custom-nav">
-				::before
-				<div class="container">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-Menu" aria-expanded="false">
-						<span class="sr-only">Toggle Navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="nav- bar brand" href="/">Tanisha Purnell</a>
-					::after
-				</div>
-			</nav>
+	<body class="pb-5">
+		<header>
+			<div class="container">
 
+				<!--Navigation bar-->
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<a class="navbar-brand" href="#"></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<!---Navbar links-->
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav ml-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">About Me</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Portfolio</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Contact Me</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
 		</header>
 
+		<!--Jumbotron-->
+		<section class="py-5">
+			<div class="container">
+				<div class="jumbotron">
+					<div class="text-center">
+					<h1>Tanisha Purnell</h1>
+						<p>Full stack Web Developer</p>
+					<p class="lead"></p>
+				</div>
+			</div>
+		</div>
+		</section>
 
+		<section class="bg-secondary text-white py-5">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3">
+						<img class="img-fluid mx-auto mx-lg-0 d-block mb-3" src="/images" alt="">
+					</div>
+					<div class="col-lg-9 text-justify">
+						<p></p>
+						<p></p>
+					</div>
+				</div>
+			</div>
+		</section>
 		<!--jQuery, Popper.js and Bootstrap Javascript-->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
