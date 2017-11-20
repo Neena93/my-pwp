ae88fb73c1d9e8b2f1ac549899684226ce2f0824
Tanisha Purnell <tanishad93@gmail.com>
2017-11-19 17:06:21 -0700

    commiting just because i can? duh

--- public_html/index.php
+++ public_html/index.php
@@ -45,7 +45,7 @@
 		</header>
 
 		<!--Jumbotron-->
-		<section class="py-5">
+		<section class="jumbotron bg-dark mb-0">
 			<div class="container">
 				<div class="jumbotron">
 					<div class="text-center">
@@ -61,15 +61,72 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-3">
-						<img class="img-fluid mx-auto mx-lg-0 d-block mb-3" src="images/hiking.jpg" alt="">
+						<img class="img-fluid mx-auto mx-lg-0 d-block mb-3" src="hiking.jpg" alt="">
 					</div>
 					<div class="col-lg-9 text-justify">
-						<p></p>
-						<p></p>
+						<h1>About Me</h1>
+						<p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.
+						</p>
+						<p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.
+						</p>
 					</div>
 				</div>
 			</div>
 		</section>
+
+		<section class="py-5">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 col-lg-3 my-3">
+						<img class="img-fluid rounded-circle d-block mx-auto my-3" src="" alt="">
+						<div class="card text-center">
+							<div class="card-body">
+								<h4 class="card-title">Card title</h4>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">click</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-lg-3 my-3">
+						<img class="img-fluid rounded-circle d-block mx-auto my-3" src="" alt="">
+						<div class="card text-center">
+							<div class="card-body">
+								<h4 class="card-title">Card title</h4>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">click</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-lg-3 my-3">
+						<img class="img-fluid rounded-circle d-block mx-auto my-3" src="" alt="">
+						<div class="card text-center">
+							<div class="card-body">
+								<h4 class="card-title">Card title</h4>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">click</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-lg-3 my-3">
+						<img class="img-fluid rounded-circle d-block mx-auto my-3" src="" alt="">
+						<div class="card text-center">
+							<div class="card-body">
+								<h4 class="card-title">Card title</h4>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">click</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer class="bg-dark fixed-bottom py-3">
+			<div class="container">
+				<div class="text-center lead text-white">Neena is my mufucking name hahaha bs </div>
+			</div>
+		</footer>
+
 		<!--jQuery, Popper.js and Bootstrap Javascript-->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
